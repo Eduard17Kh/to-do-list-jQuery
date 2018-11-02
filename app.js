1f019bd79d52688adb7304567d0d7bdc2cb28d58
Eduard <edixx@mail.ru>
2018-11-02 15:52:13 +0500

    add start node.js

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ var express = require("express"),
         res.render("html");
     });
 
-    app.listen(3000, function(){
+    app.listen(process.env.PORT || 3000, function(){
         console.log("Server has started");
     });
